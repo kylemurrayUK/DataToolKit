fbb02ebe49fdb2a8458bbc01f61876d9481e0b79
Kyle Murray <kyle.w.murray@outlook.com>
2026-01-26 23:47:42 +0000

    Set up Main method in program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace DataToolKitApp
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What Program would you like to use:");
+    }
+}
+}
+
